5e9c93c578015c62149ea56bcf402366946e3dbe
nknighta <n.knight.pc0627@gmail.com>
2023-07-30 18:06:36 +0900

    update home page

---
+++ layout/indexpage/indexElementLayout.tsx
@@ -0,0 +1,24 @@
+import {useEffect, useState} from "react";
+
+export default function IndexElementLayout() {
+    const [width, setWidth] = useState<number>(0);
+    useEffect(() => {
+        setWidth(window.innerWidth);
+        window.addEventListener("resize", () => {
+            setWidth(window.innerWidth);
+        })
+    },[width]);
+    const padding = width > 968 ? 100 : 20;
+    return (
+        <div style={{
+            fontFamily: `"REM", sans-serif`,
+            fontSize: 50,
+            wordWrap: "break-word",
+            width: "70vh",
+            padding : padding,
+        }}>
+            Low cost and short time <wbr/>
+            build Web3 Application for AnyWhere devices.
+        </div>
+    )
+};
---
+++ pages/_document.tsx
@@ -0,0 +1,67 @@
+import {Html,Main,NextScript,Head} from "next/document";
+import React, {useEffect} from "react";
+import Link from "next/link";
+import {Box,Text} from "@chakra-ui/react";
+
+const MyDocument = () => {
+    return (
+        <Html>
+            <Head/>
+            <body style={{
+                height: "auto",
+                background: "#000021",
+                color: "#fff",
+            }}>
+                <Main/>
+                <NextScript/>
+            </body>
+            <Footer/>
+        </Html>
+    )
+};
+
+const Footer = () => {
+    const [width, setWidth] = React.useState<number>(0);
+    useEffect(() => {
+        setWidth(window.innerWidth);
+        window.addEventListener("resize", () => {
+            setWidth(window.innerWidth);
+        });
+
+    },[width]);
+    const date = new Date();
+    const year = date.getFullYear();
+    return (
+        <footer>
+            <div style={{
+                padding: 10,
+                margin: 20,
+                display: "flex",
+                justifyContent: "center",
+            }}>
+                <Box>
+                    <Box>
+                        Nknight AMAMIYA © 2023 - {year}
+                    </Box>
+                    <Link href={"https://github.com/nknighta/"}>
+                        <Text p={5}>
+                            GitHub
+                        </Text>
+                    </Link>
+                    <Link href={"https://twitter.com/ama_dev_1/"}>
+                        <Text p={5}>
+                            Twitter
+                        </Text>
+                    </Link>
+                    <Link href={"https://youtube.com/@ama_p213/"}>
+                        <Text p={5}>
+                            Youtube
+                        </Text>
+                    </Link>
+                </Box>
+            </div>
+        </footer>
+    )
+};
+
+export default MyDocument;
