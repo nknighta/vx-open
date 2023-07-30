0edc81ecfa935918310524988c9929f2916fcea5
nknighta <n.knight.pc0627@gmail.com>
2023-07-30 21:21:42 +0900

    fix head

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,14 @@
 import {ReactNode} from "react";
 import ThreeBox from "components/threebox";
-import HMeta from "components/headmeta";
 import Layout from "layout/main";
 import IndexElementLayout from "layout/indexpage/indexElementLayout";
 import {GltfLoadElement} from "../components/gltfLoadElement";
 
-export const MetaDataElement = () => {
-    return (
-        <HMeta pageTitle={"Home"} pagePath={"/"} pageImg={"/header.png"}/>
-    )
-};
 
 const Home = () => {
     return (
         <>
             <link href="https://fonts.googleapis.com/css2?family=REM:wght@500&display=swap" rel="stylesheet"></link>
-            <MetaDataElement/>
             <ThreeBox/>
             <IndexElementLayout/>
             <GltfLoadElement/>
