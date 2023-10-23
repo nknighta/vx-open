bdb0fa896afd26c57696123eab0db3402f223d2b
nknighta <n.knight.pc0627@gmail.com>
2023-10-23 17:30:38 +0900

    fix: meta data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,20 @@
-import {ReactNode} from "react";
+import { ReactNode } from "react";
 import ThreeBox from "components/threebox";
 import Layout from "layout/main";
 import IndexElementLayout from "layout/indexpage/indexElementLayout";
-import {GltfLoadElement} from "../components/gltfLoadElement";
-import {Center} from "@chakra-ui/react";
-
+import { GltfLoadElement } from "../components/gltfLoadElement";
+import { Center } from "@chakra-ui/react";
+import HMeta from "components/headmeta";
 const Home = () => {
     return (
         <>
+            <HMeta pageTitle="Home"
+                pageDescription="VARIUS development team"
+                pageImg={"/header.png"} />
             <link href="https://fonts.googleapis.com/css2?family=REM:wght@500&display=swap" rel="stylesheet"></link>
-            <ThreeBox/>
-            <IndexElementLayout/>
-            <GltfLoadElement/>
+            <ThreeBox />
+            <IndexElementLayout />
+            <GltfLoadElement />
         </>
     );
 };
@@ -19,7 +22,7 @@ const Home = () => {
 Home.getLayout = (page: ReactNode) => {
     return (
         <Layout>
-            <Home/>
+            <Home />
             <Center p={10} fontSize={20}>
                 make platform, stay tune...
             </Center>
