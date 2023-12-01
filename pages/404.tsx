d75710ece8c89fdf05c64d92c88a73e4a361a7a3
nknighta <n.knight.pc0627@gmail.com>
2023-12-01 21:09:09 +0900

    refactor: fix ui

---
+++ pages/404.tsx
@@ -0,0 +1,7 @@
+export default function Custom404() {
+    return (
+        <>
+            Not Found page Back to <a href="/">Home</a>
+        </>
+    )
+};
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,6 @@ import {Html,Main,NextScript,Head} from "next/document";
 import React, {useEffect} from "react";
 import { Analytics } from '@vercel/analytics/react';
 import HMeta from "components/headmeta";
-import {Box, Text} from "@chakra-ui/react";
-import Link from "next/link";
 
 const MyDocument = () => {
     // test mode
@@ -29,13 +27,7 @@ const MyDocument = () => {
                 color: "#fff",
                 margin: "0 auto"
             }}>
-                <div style={{
-                    margin: "0 auto",
-                    padding: width > 1000 ? "1vh 4vh" : "1vh 2vh",
-                }}>
-            
                     <Main/>
-                </div>
                 <NextScript/>
             </body>
         </Html>
