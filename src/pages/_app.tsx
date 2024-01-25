aa63c6eb8d56d3e6c64fa6404d2316b47e0d2f40
nknighta <n.knight.pc0627@gmail.com>
2024-01-25 12:44:51 +0900

    perf: ⚡️ detele lint tag on _app.tsx
    detele link tag for font.google.com css fetching
    
    BREAKING CHANGE: 🧨 no breack changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -20,17 +20,8 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
             return page;
         });
     return getLayout(
-        <div style={{
-            margin: 0,
-            padding: 0,
-            fontFamily: '"Noto Sans JP", sans-serif',
-        }}>
-            <link
-                href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300&display=swap"
-                rel="stylesheet"></link>
             <Providers>
                 <Component {...pageProps} />
             </Providers>
-        </div>,
     );
 }
