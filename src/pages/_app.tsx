f66c8afb3561f58bdc179d3aadda7a48e0b8b875
nknighta <n.knight.pc0627@gmail.com>
2024-02-18 21:50:22 +0900

    fix text

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import Providers from 'components/provider';
 import * as gtag from 'scripts/gtag';
 import { useRouter } from 'next/router'
 import Script from 'next/script';
+import Providers from 'components/provider';
 // fixed Generic type 'AppProps<P, IP, C>' requires 3 type argument(s).
 type AppPropsWithLayout = AppProps & {
     Component: NextPage & {
@@ -34,21 +35,19 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
         }
     }, [])
     return getLayout(
-<<<<<<< HEAD
+        <Providers>
         <div style={{
             margin: 0,
             padding: 0,
             fontFamily: '"Noto Sans JP", sans-serif'
         }}>
-=======
-        <Providers>
+
             <Script async src="https://www.googletagmanager.com/gtag/js?id=G-18XMLEQQ2G"></Script>
             <Script id="gtag" defer dangerouslySetInnerHTML={{
                 __html: `window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());`
             }}></Script>
->>>>>>> f4b2ab98c30e945b901e64c54ad124aa5e9b9353
             <Component {...pageProps} />
         </div>,
         </Providers>
